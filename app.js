01ea65f5eabf049c6205ad3171cf3a92177ff2c6
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-07-30 17:51:48 +0100

    Export app

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+module.exports = app;
