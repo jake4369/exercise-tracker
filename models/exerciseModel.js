6d7901323966a34c6238064a5d8d524902b1accc
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-07-30 21:07:41 +0100

    exercise model

---
+++ models/exerciseModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const exerciseSchema = new mongoose.Schema({
+  description: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: () => new Date(),
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
+module.exports = Exercise;
