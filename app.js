6b390bf382104089c9ae43418180efa6dab556d1
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-07-30 18:47:14 +0100

    Require userRouter

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const userRouter = require("./routes/userRoutes");
 
+// cors middleware
 app.use(cors());
+
+// Body parsing middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static views
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+app.use("/api/users", userRouter);
 
 module.exports = app;
