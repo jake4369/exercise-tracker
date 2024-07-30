e77980fd89836ea7fb8e3d2861f52f8aa8d4dc1e
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-07-30 21:40:30 +0100

    versionKey false

--- models/userModel.js
+++ models/userModel.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
   },
-});
+  { versionKey: false }
+);
 
 const User = mongoose.model("User", userSchema);
 
