9082e8f43d9ab635dbe4988cf7d2f53077b8bc22
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-07-30 19:04:24 +0100

    Create new user

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,5 +1,30 @@
-const createUser = (req, res) => {
-  console.log(req.body);
+const User = require("./../models/userModel");
+
+const createUser = async (req, res) => {
+  try {
+    const { username } = req.body;
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      res.json({
+        message: "Username is already taken",
+      });
+    }
+
+    const newUser = await User.create({
+      username,
+    });
+
+    res.json({
+      username: newUser.username,
+      _id: newUser._id,
+    });
+  } catch (error) {
+    res.json({
+      error: "Internal server error",
+    });
+  }
 };
 
 module.exports = {
