0e06c23cb8eb29da79f6e6f41bd8f2a0919c6015
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-07-30 21:08:17 +0100

    Create exercise route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const { createUser, getUsers } = require("./../controllers/userControllers");
+const { createExercise } = require("./../controllers/exerciseControllers");
 
 const router = express.Router();
 
 router.route("/").post(createUser).get(getUsers);
+
+router.route("/:_id/exercises").post(createExercise);
 
 module.exports = router;
