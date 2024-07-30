c318ff55642375ac433023fd3e3e8cf8e4b4f136
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-07-30 18:54:30 +0100

    User model

---
+++ models/userModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
