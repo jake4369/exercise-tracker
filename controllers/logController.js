cc833df4cfe8ee5b53202a848665a56c6896de2e
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-07-31 15:41:24 +0100

    Add from, to and limit parameters to a GET /api/users/:_id/logs request

--- controllers/logController.js
+++ controllers/logController.js
@@ -3,6 +3,7 @@ const User = require("./../models/userModel");
 const getLogs = async (req, res) => {
   try {
     const user_id = req.params._id;
+    const { limit, from, to } = req.query;
 
     const user = await User.findById(user_id);
 
@@ -12,11 +13,41 @@ const getLogs = async (req, res) => {
       });
     }
 
+    let userLog = user.log;
+
+    if (from) {
+      const fromDate = new Date(from);
+      fromDate.setHours(0, 0, 0, 0); // Normalize the from date to midnight
+
+      userLog = user.log.filter((obj) => {
+        const logDate = new Date(obj.date);
+        logDate.setHours(0, 0, 0, 0); // Normalize each log date to midnight
+
+        return logDate.getTime() >= fromDate.getTime();
+      });
+    }
+
+    if (to) {
+      const toDate = new Date(to);
+      toDate.setHours(23, 59, 59, 999); // Normalize the to date to the end of the day
+
+      userLog = user.log.filter((obj) => {
+        const logDate = new Date(obj.date);
+        logDate.setHours(0, 0, 0, 0); // Normalize each log date to midnight
+
+        return logDate.getTime() <= toDate.getTime();
+      });
+    }
+
+    if (limit) {
+      userLog = user.log.slice(0, limit);
+    }
+
     const responseObj = {
       _id: user._id,
       username: user.username,
-      count: user.log.length,
-      log: user.log,
+      count: userLog.length,
+      log: userLog,
     };
 
     return res.json(responseObj);
