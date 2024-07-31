2d30dfc3d3b40e1302f367d3f5884d9da2ba8379
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-07-31 12:33:25 +0100

    Get user log route - GET

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const { createUser, getUsers } = require("./../controllers/userControllers");
 const { createExercise } = require("./../controllers/exerciseControllers");
+const { getLogs } = require("./../controllers/logController");
 
 const router = express.Router();
 
 router.route("/").post(createUser).get(getUsers);
 
 router.route("/:_id/exercises").post(createExercise);
+
+router.route("/:_id/logs").get(getLogs);
 
 module.exports = router;
